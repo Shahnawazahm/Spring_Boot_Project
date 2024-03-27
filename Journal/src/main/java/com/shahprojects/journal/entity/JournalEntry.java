cc5e6a415e83183ec57efeac216e1ee5c660e053
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-03-27 23:17:55 +0530

    Update JournalEntry.java

--- Journal/src/main/java/com/shahprojects/journal/entity/JournalEntry.java
+++ Journal/src/main/java/com/shahprojects/journal/entity/JournalEntry.java
@@ -1,15 +1,25 @@
 package com.shahprojects.journal.entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document
 public class JournalEntry {
- private long id;
+    @Id
+private ObjectId id;
  private String title;
  private String content;
+ private LocalDateTime date;
 
-    public long getId() {
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
@@ -27,5 +37,13 @@ public class JournalEntry {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
     }
 }
