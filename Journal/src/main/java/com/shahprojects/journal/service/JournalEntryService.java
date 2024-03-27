8353c5fd3b57470fc31f502dacc2858a77d03ed6
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-03-27 23:19:08 +0530

    Update JournalEntryService.java

--- Journal/src/main/java/com/shahprojects/journal/service/JournalEntryService.java
+++ Journal/src/main/java/com/shahprojects/journal/service/JournalEntryService.java
@@ -2,10 +2,12 @@ package com.shahprojects.journal.service;
 
 import com.shahprojects.journal.entity.JournalEntry;
 import com.shahprojects.journal.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JournalEntryService {
@@ -19,4 +21,16 @@ public class JournalEntryService {
 public List<JournalEntry> getAll(){
   return journalEntryRepository.findAll();
 }
+public Optional<JournalEntry> findById(ObjectId myId){
+  return journalEntryRepository.findById(myId);
+
+}
+public void  deleteById(ObjectId myId){
+  journalEntryRepository.deleteById(myId);
+
+}
+//public JournalEntry updateById(JournalEntry updateEntry){
+//  return journalEntryRepository.
+//}
+
 }
