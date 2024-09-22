3c46b1e7c3119daa481fb251280e5e21e44eab13
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-09-22 17:56:53 +0530

    commited

---
+++ Bus/security/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.redbus.security.repository;
+
+import com.redbus.security.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
+
