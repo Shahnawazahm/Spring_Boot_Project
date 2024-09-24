f08d0a109ba8e1d30ab05b5d559e38228cb258f8
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-09-24 18:20:52 +0530

    commited

---
+++ Bus/RedbusApplication.java
@@ -0,0 +1,13 @@
+package com.redbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedbusApplication.class, args);
+	}
+
+}
