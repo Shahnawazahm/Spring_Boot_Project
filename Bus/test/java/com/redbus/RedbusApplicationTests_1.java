cd9068be46d9ad3e87e39806a682a8cae34df2d1
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-09-29 18:46:47 +0530

    commited

---
+++ Bus/test/java/com/redbus/RedbusApplicationTests_1.java
@@ -0,0 +1,13 @@
+package com.redbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
